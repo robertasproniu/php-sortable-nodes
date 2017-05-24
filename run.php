dc6a2d46dffebe9532931186bb2f33867ede3979
robertaproniu <robert.asproniu.work@gmail.com>
2017-05-24 10:50:41 +0300

    Minor changes

--- run.php
+++ run.php
@@ -7,7 +7,7 @@ use NodeSetCollection\NodeCollection;
 
 function run()
 {
-    $collection1 = NodeCollection::create('a/1, a/2, a/3, a/a, a/128, a/129, b/65, b/66, c/1, c/10, c/42');
+    $collection1 = NodeCollection::create('a/1, a/2, a/3, a/4, a/128, a/129, b/65, b/66, c/1, c/10, c/42');
 
     $collection2 = NodeCollection::create('a/1, a/2, a/3, a/4 a/5, a/126, a/127, b/100, c/2, c/3, d/1');
 
